620c8c5fbac9cf6a0390fa77780f4b8a0558a01a
Robert Zuchnicki <robert.zuchnicki@gmail.com>
2021-10-02 16:49:34 +0200

    rozwiazanie konfliktu

--- ProjektDom/src/pakietA/Main.java
+++ ProjektDom/src/pakietA/Main.java
@@ -6,10 +6,6 @@ public class Main {
         System.out.println("Hello word!");
           System.out.println("Witaj Świecie");
          System.out.println("Witaj Polsko!");
-<<<<<<< HEAD
-         //JESTEM NA BRANCHU MAIN
-=======
         //dodaje ten komentarz w zdalnym repo!
->>>>>>> 4a8c9cfc134d9efcfd0fd514eb3c47d0020552cf
     }
 }
