82a1278adfc0cc8a63430c0f4b3ef5df2bd0e064
zuchnickiR <85823318+zuchnickiR@users.noreply.github.com>
2021-10-02 16:24:47 +0200

    Update Main.java

--- ProjektDom/src/pakietA/Main.java
+++ ProjektDom/src/pakietA/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello word!");
           System.out.println("Witaj Świecie");
+         System.out.println("Witaj Polsko!");
     }
 }
